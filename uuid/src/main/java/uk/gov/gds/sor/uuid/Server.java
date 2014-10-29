d9783abe6b79d75bc43ceb27afe0f7f436759ad8
Mat Wall <mat.wall@digital.cabinet-office.gov.uk>
2014-10-29 13:52:42 +0000

    Switching to scala

--- uuid/src/main/java/uk/gov/gds/sor/uuid/Server.java
+++
@@ -1,9 +0,0 @@
-package uk.gov.gds.sor.uuid;
-
-import static spark.Spark.get;
-
-class Server {
-    public static void main(String[] args) {
-        get("/foo", (request, response) -> "Foo");
-    }
-}
