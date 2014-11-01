ed0669bdb50f11697f6720e90d359ab4cb03ecb7
Mat Wall <mat.wall@digital.cabinet-office.gov.uk>
2014-11-01 17:57:55 +0000

    Changed to scala 2.11.2

--- project/SystemOfRecordBuild.scala
+++ project/SystemOfRecordBuild.scala
@@ -15,6 +15,7 @@ object SystemOfRecordBuild extends Build {
 object GdsProject {
   def apply(name: String) = Project(name, file(name))
     .settings(scalacOptions := Seq("-unchecked", "-deprecation", "-encoding", "utf8"))
+    .settings(scalaVersion := "2.11.2")
 }
 
 object SprayServer {
